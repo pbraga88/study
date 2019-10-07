4a2d39504a4e9ff9209e2c1e15c2e080c4209784
pbraga <paulo.braga1388@gmail.com>
2019-10-07 14:46:37 -0700

    Sync

---
+++ C-Programming/III-AdvancedDataTypes/3-LinkedLists/6.0_SearchForA-NodeInA-LinkedList/Class/src/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct digit {
+    int num;
+    struct digit *next;
+};
+
+struct digit * createDigit(int);
+struct digit * append(struct digit * end, struct digit * newDigptr);
+void printNumber(struct digit *);
+void freeNumber(struct digit *start);
+struct digit * readNumber();
+struct digit *searchNumber(struct digit *start,int number);
+
+int main(void) {
+    int nb = 5;
+    struct digit *start, *ptr;
+    printf("Please enter a number: ");
+    start = readNumber();
+    ptr = searchNumber(start, nb);
+    if (ptr != NULL) {
+        printf("Number %d found at memory location %p.\n", nb, (void*)ptr);
+    }
+    else {
+        printf("Number %d not found.\n", nb);
+    }
+
+    printNumber(start);
+    freeNumber(start);
+    return 0;
+}
+
+struct digit *createDigit(int dig) {
+    struct digit *ptr;
+    ptr = (struct digit *) malloc(sizeof(struct digit));
+    ptr->num = dig;
+    ptr->next = NULL;
+    return ptr;
+}
+
+struct digit * append(struct digit * end, struct digit * newDigptr) {
+    end->next = newDigptr;
+    return(end->next);
+}
+
+void printNumber(struct digit *start) {
+    struct digit * ptr = start;
+    while (ptr!=NULL) {
+        printf("%d", ptr->num);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+void freeNumber(struct digit *start) {
+    struct digit * ptr = start;
+    struct digit * tmp;
+    while (ptr!=NULL) {
+        tmp = ptr->next;
+        free(ptr);
+        ptr = tmp;
+    }
+}
+
+struct digit * readNumber() {
+    char c;
+    int d;
+    struct digit *start, *end, *newptr;
+    start = NULL;
+    scanf("%c", &c);
+    while (c!='\n') {
+        d = c - 48;
+        newptr = createDigit(d);
+        if (start==NULL) {
+            start = newptr;
+            end = start;
+        } else {
+            end = append(end, newptr);
+        }
+        scanf("%c", &c);
+    }
+    return start;
+}
+
+struct digit *searchNumber(struct digit *start,int number)
+{
+    while(start != NULL && start->num != number) {
+        start = start->next;
+    } 
+    return start;
+}
