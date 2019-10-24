c52f292759bef67fd68454b3d87fa64bbfe2fb6f
pbraga <paulo.braga1388@gmail.com>
2019-10-24 12:43:13 -0700

    Sync

---
+++ CPA_cpp/1_IntroductionToComputerProgramming/1.4_NumbersAndTheirOperators/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 4;
+	float PI, Field;
+	PI = 10.0;
+	Field = 4.0;
+
+	float result = 1e10;
+	x = result;
+	cout<< result <<endl;
+	cout<< x <<endl;
+
+	return 0;
+}
