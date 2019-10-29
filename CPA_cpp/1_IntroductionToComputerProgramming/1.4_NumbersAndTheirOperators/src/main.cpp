1f85efae3e8e4dcdc807e46507595f2e6d0d87be
Paulo <paulo.braga1388@gmail.com>
2019-10-29 20:52:40 -0300

    Sync

--- CPA_cpp/1_IntroductionToComputerProgramming/1.4_NumbersAndTheirOperators/src/main.cpp
+++ CPA_cpp/1_IntroductionToComputerProgramming/1.4_NumbersAndTheirOperators/src/main.cpp
@@ -3,16 +3,29 @@
 using namespace std;
 
 int main() {
-	int x = 10;
-	int y = 4;
-	float PI, Field;
-	PI = 10.0;
-	Field = 4.0;
+	// int k;
+	// int x = 10;
+	// int y = 4;
+	// k = x%y;
 
-	float result = 1e10;
-	x = result;
-	cout<< result <<endl;
-	cout<< x <<endl;
+	// float PI, Field;
+	// PI = 10.0;
+	// Field = 4.0;
+
+	// float result = 1e10;
+	// x = result;
+	// cout<< result <<endl;
+	// cout<< x <<endl;
+
+	// cout<< k << endl;
+
+	int i = 1;
+	int y;
+	y = i++;
+	cout<< y << ' ' << i <<endl;
+
+	y = ++i;
+	cout<< y << ' ' << i <<endl;
 
 	return 0;
 }
