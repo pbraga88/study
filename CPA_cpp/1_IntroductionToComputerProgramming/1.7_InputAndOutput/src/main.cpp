cee700cf6bfc4782e4f94a341d46e0a197f48ffe
Paulo <paulo.braga1388@gmail.com>
2019-11-02 11:18:42 -0300

    sync

---
+++ CPA_cpp/1_IntroductionToComputerProgramming/1.7_InputAndOutput/src/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	// float a = 2.5;
+	// float b = 0.0000000025;
+
+	// char x = 'X';
+	// int y = x;
+	// int byte = 255;
+	// cout << "Byte in hex: " << hex << byte << endl;
+	// cout << dec << byte << endl;
+	
+	// cout << "using the function setbase(8): " << setbase(8) << byte << endl;
+	// cout << "using the function setbase(10): " << setbase(10) << byte << endl;
+	// cout << "using the function setbase(16): " << setbase(16) << byte << endl;
+
+	// cout << "Typecasting the var x=X into it's decimal ASCII value: "\
+	// 	 << dec << (int)x << endl;
+	// cout << "Typecasting the var y=x \n into it's character correspondent: "\
+	// 	 << (char)y << endl;
+
+	// cout << fixed << a << " " << b << endl;
+ // 	cout << scientific << a << " " << b << endl;
+
+	// float value;
+
+	// cout << "Enter a number: " << endl;
+	// cin >> value;
+
+	// if(value>=0)
+	// 	cout << "Square Root: " << sqrtf(value) << endl;
+	// else
+	// 	cout << "Enter a non-negative value" << endl;
+
+	int i, j, k;
+	i = 10;
+	j = 3;
+	k = i%j;
+	cout << k << endl;
+
+	return 0;
+}
