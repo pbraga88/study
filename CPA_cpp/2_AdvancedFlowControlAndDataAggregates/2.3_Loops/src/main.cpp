1b0dc9816af49922455001ee9573353f2e39db6a
pbraga <paulo.braga1388@gmail.com>
2019-11-04 16:06:52 -0800

    Sync

---
+++ CPA_cpp/2_AdvancedFlowControlAndDataAggregates/2.3_Loops/src/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// int number;
+	// int max;
+	// int even = 0;
+	// int odd = 0;
+
+	// cin >> number;
+	// max = number;
+
+	// while (number) {
+	// 	if (number > max)
+	// 		max = number;
+
+	// 	if(number % 2)
+	// 		odd++;
+	// 	else
+	// 		even++;
+
+	// 	cin >> number;
+	// } 
+
+	// cout << "The largest number is:" << max << endl;
+	// cout << "The of odd numbers are:" << odd << endl;
+	// cout << "The qty of even numbers are:" << even << endl;
+
+/*=================================================================*/
+	// int number=0;
+	// int max=-100000;
+	// int counter = 0;
+	// do{
+	// 	cin >> number;
+	// 	if(number != -1)
+	// 		counter++;
+	// 	if(number > max)
+	// 		max = number;
+	// }
+	// while(number != -1);
+
+	// if (!counter)
+	// 	cout << "You didn't enter any valid value" << endl;
+	// else
+	// 	cout << "Numbers qty entered: " << counter << endl;
+
+/*=================================================================*/
+	// int counter = 0;
+	// while (++counter < 10)
+	// 	cout << counter << endl;
+
+/*=================================================================*/
+	// int pow = 1;
+	// int counter;
+	// for (counter = 0; counter<=16; counter++) {
+	// 	cout << "2^" << counter << "=" << pow << endl;
+	// 	pow *= 2;
+	// }
+
+/*=================================================================*/
+	// int i = 0;
+	// for(;;) {
+	// 	cin>>i;
+	// 	if(!i)
+	// 		break;
+	// }	
+	// cout << "You left the loop" << endl;
+
+/*=================================================================*/
+	int i;
+	do {
+		cin >> i;
+		if(!i)
+			continue;
+		cout << i << endl;
+	}
+	while(i) ;
+	cout << "You left the loop" << endl;
+	return 0;
+}
